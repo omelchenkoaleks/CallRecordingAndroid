1ba07d7b67dd2668e4f4a814805d5d9d0949019a
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-11-27 06:58:00 +0200

    to do

--- app/src/main/java/com/omelchenkoaleks/callrecordingandroid/RecordingService.java
+++ app/src/main/java/com/omelchenkoaleks/callrecordingandroid/RecordingService.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -15,7 +18,7 @@ public class RecordingService extends Service {
     private MediaRecorder rec;
     private boolean recordsStarted;
     private File file;
-    String path = "sdcard/alarms";
+    String path = "/sdcard/alarms";
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -28,13 +31,48 @@ public class RecordingService extends Service {
 
         file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_ALARMS);
 
+
         Date date = new Date();
-        CharSequence sdf = DateFormat.format("MM-dd-yy-hh-mm-ss", date.getTime());
+        CharSequence sdf =
+                android.text.format.DateFormat.format("MM-dd-yy-hh-mm-ss", date);
 
         rec = new MediaRecorder();
         rec.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
         rec.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         rec.setOutputFile(file.getAbsolutePath() + "/" + sdf + "rec.3gp");
         rec.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        TelephonyManager manager = (TelephonyManager) getApplicationContext()
+                .getSystemService(getApplicationContext().TELEPHONY_SERVICE);
+
+        manager.listen(new PhoneStateListener() {
+
+            @Override
+            public void onCallStateChanged(int state, String phoneNumber) {
+//                super.onCallStateChanged(state, phoneNumber) {
+
+                    if (TelephonyManager.CALL_STATE_IDLE == state && rec == null) {
+
+                        rec.stop();
+                        rec.reset();
+                        rec.release();
+                        recordsStarted = false;
+                        stopSelf();
+
+                    } else if (TelephonyManager.CALL_STATE_OFFHOOK == state) {
+
+                        try {
+                            rec.prepare();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        rec.start();
+                        recordsStarted = true;
+                    }
+                }
+
+        }, PhoneStateListener.LISTEN_CALL_STATE);
+
+        return START_STICKY;
     }
 }
